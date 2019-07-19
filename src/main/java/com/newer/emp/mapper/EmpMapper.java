e34129b89f6a0f934a98ba8906bef6c6f84136db
WHSXHNA <1429827953@qq.com>
2019-07-19 21:07:07 +0800

    廖艾提交

--- src/main/java/com/newer/emp/mapper/EmpMapper.java
+++ src/main/java/com/newer/emp/mapper/EmpMapper.java
@@ -2,8 +2,13 @@ package com.newer.emp.mapper;
 
 import com.newer.emp.domain.Emp;
 import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 public interface EmpMapper {
     @Insert("insert into newemp values(#{empno},#{ename},#{job},#{mgr},#{hiredate},#{sal},#{comm},#{deptno})")
     public int addEmp(Emp emp);
+
+    public List<Emp> select(@Param("ename") String ename);
 }
